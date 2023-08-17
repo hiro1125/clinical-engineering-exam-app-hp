4ac787764d9d8bde25c547e4dc7a4c70bd336ab5
hiro1125 <hiro112538@gmail.com>
2023-08-17 21:58:19 +0900

    feat: モックアップ画像の追加

--- app/components/Main/Main.tsx
+++ app/components/Main/Main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Image from 'next/image';
+import topScreen from '/app/Images/top-screen.jpg';
 
 const Main = () => {
   return (
-    <div className='w-full bg-gray-200'>
-      <div>aaaaa</div>
+    <div className=' bg-gray-200'>
+      <Image src={topScreen} width={500} height={500} alt='top-screen' />
     </div>
   );
 };
