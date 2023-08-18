1c6d79dd5a94a256f3359f6d24951f6a9465c54f
hiro1125 <hiro112538@gmail.com>
2023-08-18 12:49:43 +0900

    feat: 一覧画像の仮作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from '@/app/components/Header/Header';
 import Main from '@/app/components/Main/Main';
+import Overview from '@/app/components/Overview/Overview';
 
 export default function Home() {
   return (
     <main>
       <Header title={'臨床工学技士国家試験'} />
       <Main />
+      <Overview />
     </main>
   );
 }
