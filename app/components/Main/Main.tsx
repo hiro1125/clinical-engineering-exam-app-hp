e67236662907e439f8b5627699bd1cca34b89f0e
hiro1125 <hiro112538@gmail.com>
2023-08-18 12:47:55 +0900

    feat: Textを追加

--- app/components/Main/Main.tsx
+++ app/components/Main/Main.tsx
@@ -4,8 +4,18 @@ import topScreen from '/app/Images/top-screen.jpg';
 
 const Main = () => {
   return (
-    <div className=' bg-gray-200'>
-      <Image src={topScreen} width={500} height={500} alt='top-screen' />
+    <div className='flex items-center justify-center'>
+      <div className='flex'>
+        <Image src={topScreen} width={700} height={700} alt='top-screen' />
+        <div className='w-1/2 flex flex-col items-center justify-center'>
+          <p className='text-7xl font-semibold text-center mb-4'>
+            国家試験対策
+          </p>
+          <p className='text-3xl text-center'>
+            臨床工学技士試験対策アプリです。
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
