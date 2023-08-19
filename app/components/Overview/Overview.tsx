e4c4089aca9a93ca03bd8c38e247abf478a0cead
hiro1125 <hiro112538@gmail.com>
2023-08-20 08:32:45 +0900

    feat: textの追加

--- app/components/Overview/Overview.tsx
+++ app/components/Overview/Overview.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Overview = () => {
   return (
-    <div>
-      <div>Overview</div>
+    <div className='flex'>
+      <div className='w-1/2 flex flex-col ml-60'>
+        <p className='text-7xl font-semibold mb-4'>主な画面</p>
+        <p className='text-3xl'>主要機能を画面で直接ご覧ください。</p>
+      </div>
     </div>
   );
 };
