3a08aa1f0586731ccae8286224843c6c0e92a8b7
hiro1125 <hiro112538@gmail.com>
2023-08-28 19:10:16 +0900

    feat: Footerの仮実装

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='w-full text-4xl bg-slate-300'>
+      <div className='text-black p-10'>aaaa</div>
+    </div>
+  );
+};
+
+export default Footer;
