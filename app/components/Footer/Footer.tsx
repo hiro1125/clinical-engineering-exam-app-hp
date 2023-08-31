5023f566771943e845129bd101fefc4ee626281f
hiro1125 <hiro112538@gmail.com>
2023-08-31 18:16:55 +0900

    fix: コピーライトの記載が不適切のため修正

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className='w-full p-10 text-3xl bg-slate-300 text-center mt-5'>
-      © 2023
+      © hi-sa 2023
     </div>
   );
 };
