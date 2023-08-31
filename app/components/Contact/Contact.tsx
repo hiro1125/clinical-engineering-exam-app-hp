18236012a87ee453247707947748fc914cba137c
hiro1125 <hiro112538@gmail.com>
2023-08-31 18:59:13 +0900

    feat: お問い合わせフォームの画面遷移を実装

--- app/components/Contact/Contact.tsx
+++ app/components/Contact/Contact.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Contact = () => {
@@ -5,11 +6,15 @@ const Contact = () => {
     <div className='flex flex-col justify-center items-center'>
       <p className='text-7xl font-semibold mt-20 mb-10'>お問い合わせ</p>
       <p className='text-3xl mb-10'>
-        アプリを使用して、不具合などは下記お問い合わせフォームよりお願いいたします。
+        アプリを使用して、不具合などに関して下記のお問い合わせフォームよりお願いいたします。
       </p>
-      <button className='text-2xl mb-10 rounded-full bg-blue-400 hover:bg-blue-800 text-white px-10 py-5'>
+      <Link
+        className='text-2xl mb-10 rounded-full bg-blue-400 hover:bg-blue-800 text-white px-10 py-5'
+        href={'EnquiryForm'}
+        target='_blank'
+      >
         お問い合わせはこちら
-      </button>
+      </Link>
     </div>
   );
 };
