bf514d8fc90a32fdf9ec1874bf205ab05a5f9880
hiro1125 <hiro112538@gmail.com>
2023-09-04 21:20:45 +0900

    fix: Footerの位置の不具合修正と文字の大きさの調整

--- app/EnquiryForm/page.tsx
+++ app/EnquiryForm/page.tsx
@@ -35,46 +35,50 @@ const Contact = () => {
     <>
       <div className='w-full h-full'>
         <div className='flex flex-col items-center justify-center'>
-          <h2 className='text-2xl font-bold mt-20'>お問い合わせ</h2>
-          <p className='text-base font-bold justify-center mt-8'>
+          <h2 className='text-4xl font-bold mt-32'>お問い合わせ</h2>
+          <p className='text-3xl font-bold justify-center mt-8'>
             お問い合わせは、下記フォームよりご記入の上ご連絡ください。
           </p>
-          <p className='text-base font-bold mt-5'>
+          <p className='text-3xl font-bold mt-5'>
             <span className='text-red-500'>*</span>
             は、必須事項です。
           </p>
-          <form ref={form} onSubmit={sendEmail}>
-            <label htmlFor='mailTitleForm' className='font-bold text-lg'>
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            className='font-bold text-lg my-10'
+          >
+            <label htmlFor='mailTitleForm'>
               件名<span className='text-red-500'>*</span>
             </label>
             <input
               id='mailTitleForm'
-              className='border border-gray-300 rounded px-3 py-2 w-full bg-gray-100 text-xl mb-2'
+              className='border border-gray-300 rounded px-3 py-2 w-full bg-gray-100 text-xl mb-7'
               type='text'
               placeholder='件名'
               name='subject'
               required
             />
-            <label htmlFor='mailForm' className='font-bold text-lg'>
+            <label htmlFor='mailForm'>
               メールアドレス<span className='text-red-500'>*</span>
             </label>
             <input
               id='mailForm'
-              className='border border-gray-300 rounded px-3 py-2 w-full bg-gray-100 text-xl mb-2'
+              className='border border-gray-300 rounded px-3 py-2 w-full bg-gray-100 text-xl mb-7'
               type='email'
               placeholder='例）example@gmail.com'
               name='email'
               required
             />
-            <label htmlFor='mailContentForm' className='font-bold text-lg'>
+            <label htmlFor='mailContentForm'>
               お問い合わせ内容<span className='text-red-500'>*</span>
             </label>
             <textarea
               id='mailContentForm'
-              className='border border-gray-300 rounded px-3 py-2 w-full bg-gray-100 text-xl mb-2'
+              className='border border-gray-300 rounded px-3 py-2 w-full bg-gray-100 text-xl mb-7'
               name='message'
-              cols={30}
-              rows={10}
+              cols={40}
+              rows={20}
               placeholder='お問い合わせ内容を入力してください'
             ></textarea>
             <button
@@ -102,7 +106,9 @@ const Contact = () => {
           </form>
         </div>
       </div>
-      <Footer />
+      <div className='w-full text-3xl bg-slate-300 text-center border-t absolute bottom-0'>
+        <Footer />
+      </div>
     </>
   );
 };
--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='w-full p-10 text-3xl bg-slate-300 text-center mt-5'>
+    <div className='w-full p-10 text-3xl bg-slate-300 text-center'>
       © hi-sa 2023
     </div>
   );
