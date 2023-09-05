81af62e54dac7ad481b3143cfa8d3eba1420b880
hiro1125 <hiro112538@gmail.com>
2023-09-05 20:57:24 +0900

    fix: headerが被ってしまう不具合の修正

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Header: FC<Props> = ({ title }) => {
   return (
-    <div className='fixed top-0 left-0 right-0 text-white text-4xl backdrop-blur-md'>
+    <div className='fixed top-0 left-0 right-0 text-white text-4xl backdrop-blur-md z-50'>
       <div className='text-black p-10'>{title}</div>
     </div>
   );
