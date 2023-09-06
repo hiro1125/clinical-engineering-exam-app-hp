d85a343da6eb7d379aeae52f34bd8becdd59e2b1
hiro1125 <hiro112538@gmail.com>
2023-09-06 21:45:04 +0900

    refactor: header部分のレスポンシブ対応

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -6,8 +6,10 @@ type Props = {
 
 const Header: FC<Props> = ({ title }) => {
   return (
-    <div className='fixed top-0 left-0 right-0 text-white text-4xl backdrop-blur-md z-50'>
-      <div className='text-black p-10'>{title}</div>
+    <div className='lg:fixed text-3xl lg:text-left text-center top-0 left-0 right-0 text-white backdrop-blur-md z-50'>
+      <div className='text-black lg:p-10 p-8 lg:text-4xl font-bold'>
+        {title}
+      </div>
     </div>
   );
 };
