033fab4fdacd79e699a523c96ac2577705edeee7
hiro1125 <hiro112538@gmail.com>
2023-09-07 21:09:22 +0900

    fix: レスポンシブ対応においてheaderが固定されない不具合の修正

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Header: FC<Props> = ({ title }) => {
   return (
-    <div className='lg:fixed text-3xl lg:text-left text-center top-0 left-0 right-0 text-white backdrop-blur-md z-50'>
+    <div className='fixed text-3xl lg:text-left text-center top-0 left-0 right-0 text-white backdrop-blur-md z-50'>
       <div className='text-black lg:p-10 p-8 lg:text-4xl font-bold'>
         {title}
       </div>
