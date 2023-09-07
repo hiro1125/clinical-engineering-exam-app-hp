ae2a31c4c2bb336a24bd1eb3074c13ca38674d13
hiro1125 <hiro112538@gmail.com>
2023-09-07 20:23:29 +0900

    refactor: Main部分のレスポンシブ対応

--- app/components/Main/Main.tsx
+++ app/components/Main/Main.tsx
@@ -5,13 +5,13 @@ import topScreen from '/app/Images/top-screen.jpg';
 const Main = () => {
   return (
     <div className='flex items-center justify-center mt-10'>
-      <div className='flex'>
+      <div className='lg:flex'>
         <Image src={topScreen} width={700} height={700} alt='top-screen' />
-        <div className='w-1/2 flex flex-col items-center justify-center'>
-          <p className='text-7xl font-semibold text-center mb-4'>
+        <div className='lg:w-1/2 lg:flex flex-col items-center justify-center'>
+          <p className='lg:text-7xl text-5xl font-semibold text-center lg:mb-4 m-5'>
             国家試験対策
           </p>
-          <p className='text-3xl text-center'>
+          <p className='lg:text-3xl text-xl text-center lg:mb-0 mb-5 '>
             臨床工学技士試験対策アプリです。
           </p>
         </div>
