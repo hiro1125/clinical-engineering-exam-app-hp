c86317ce82429765bc1c59e9920098c5aa6f1861
hiro1125 <hiro112538@gmail.com>
2023-09-08 21:42:19 +0900

    refactor: Footer部分のレスポンシブ対応

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='w-full p-10 text-3xl bg-slate-300 text-center mt-28'>
+    <div className='w-full p-10 text-3xl bg-slate-300 text-center lg:mt-28 mt-20'>
       © hi-sa 2023
     </div>
   );
