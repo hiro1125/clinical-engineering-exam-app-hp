7e43f93fbbe908314f512b8ff716f824e703fab6
hiro1125 <hiro112538@gmail.com>
2023-09-08 21:41:50 +0900

    refactor: Contact部分のレスポンシブ対応

--- app/components/Contact/Contact.tsx
+++ app/components/Contact/Contact.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div className='flex flex-col justify-center items-center mt-24'>
-      <p className='text-7xl font-semibold mb-10'>お問い合わせ</p>
-      <p className='text-3xl mb-10'>
+    <div className='flex flex-col justify-center items-center lg:mt-24'>
+      <p className='lg:text-7xl text-5xl font-semibold mb-10 lg:mt-0 mt-10'>
+        お問い合わせ
+      </p>
+      <p className='lg:text-3xl text-2xl lg:mb-10 lg:m-0 m-4'>
         アプリを使用して、不具合などに関して下記のお問い合わせフォームよりお願いいたします。
       </p>
       <Link
-        className='text-2xl mb-10 rounded-full bg-blue-400 hover:bg-blue-800 text-white px-10 py-5'
+        className='text-2xl lg:mb-10 mb-6 lg:mt-0 mt-6 rounded-full bg-blue-400 hover:bg-blue-800 text-white px-10 py-5'
         href={'EnquiryForm'}
         target='_blank'
       >
