77d393bb8bba963b3b775e47c92106942ccb0ea1
hiro1125 <hiro112538@gmail.com>
2023-09-10 09:07:42 +0900

    style: 画像と文字の間隔を調整

--- app/components/Main/Main.tsx
+++ app/components/Main/Main.tsx
@@ -5,8 +5,14 @@ import topScreen from '/app/Images/top-screen.jpg';
 const Main = () => {
   return (
     <div className='flex items-center justify-center mt-10'>
-      <div className='lg:flex'>
-        <Image src={topScreen} width={700} height={700} alt='top-screen' />
+      <div className='lg:flex lg:mb-10'>
+        <Image
+          src={topScreen}
+          width={700}
+          height={700}
+          alt='top-screen'
+          className='lg:mr-11'
+        />
         <div className='lg:w-1/2 lg:flex flex-col items-center justify-center'>
           <p className='lg:text-7xl text-5xl font-semibold text-center lg:mb-4 m-5'>
             国家試験対策
