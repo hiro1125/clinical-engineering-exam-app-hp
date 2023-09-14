571c4ccef226d1ca0c6bfadb72ee1e839fc40e6b
hiro1125 <hiro112538@gmail.com>
2023-09-14 22:09:12 +0900

    refactor: プライバシーポリシーの追加によるレスポンシブ対応

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,9 +1,19 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='w-full p-10 text-3xl bg-slate-300 text-center'>
-      © hi-sa 2023
+    <div className='w-full lg:p-10 p-5 lg:text-3xl bg-slate-300 text-center'>
+      <div className='flex gap-12 justify-center items-center lg:text-3xl text-lg font-medium'>
+        © hi-sa 2023
+        <Link
+          className='lg:text-2xl hover:underline'
+          href={'PrivacyPolicy'}
+          target='_blank'
+        >
+          プライバシーポリシー
+        </Link>
+      </div>
     </div>
   );
 };
